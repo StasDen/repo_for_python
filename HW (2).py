132c8f89a3bd474732f30d4d24125a9d19aa6d8e
Stas Denysenko <lohopusik67@gmail.com>
2022-01-31 20:15:20 +0200

    Update HW (2).py

--- HW (2).py
+++ HW (2).py
@@ -10,7 +10,7 @@ class Student:
 
     def __init__(self, first_name: str = "", last_name: str = "") -> None:
         self.first_name = first_name    # public
-        self.last_name = last_name      
+        self.last_name = last_name
         self.__name = "hidden"          # private
         self._another = "another"       # protected
 
@@ -27,8 +27,12 @@ class Dean:
     pass
 
 
-class Fish: 
-    def sell_fish(self, name_of_fish): # Relevant function if only I was a fish shop
+class Fish:
+    age_in_mounths_of_fish = 17.5
+    weight_of_fish_in_kg = 3.2
+    fish_names_list = ["Salmon", "Tuna", "Carp", "Cod", "Swordship"] # Example of a list(list is ordered, changeable)
+
+    def sell_fish(self, name_of_fish):  # Relevant function if only I was a fish shop
         self.name_of_fish = name_of_fish
 
     def print_sold_fish(self, sold_fish: str) -> None:
@@ -36,7 +40,7 @@ class Fish:
         self.sentence_about_sold_fish = "2 kg of " + sold_fish + " have been sold."
         print(self.sentence_about_sold_fish)
 
-    def buy_fish(self, name_of_fish): # Same function as above
+    def buy_fish(self, name_of_fish):  # Same function as above
         self.name_of_fish = name_of_fish
 
     def print_bought_fish(self, bought_fish: str) -> None:
@@ -44,16 +48,54 @@ class Fish:
         self.sentence_about_bought_fish = "3 boxes of " + bought_fish + " have been bought."
         print(self.sentence_about_bought_fish)
 
-    def __init__(self) -> None:
+    def __init__(self, name_of_fish) -> None:
+        self.name_of_fish = name_of_fish
+
         self.name = "oseledets"
         self.price_in_uah_per_kilo = 11.2
-        # self.catch_date = datetime("21/01/2022") ?Isn't working
+        self.catch_date = datetime.date(2022, 1, 21) # !datetime.SOMETHING(date, time, datetime) is working
         self.origin = "Norway"
         self.body_only = True
         self.weight = 100
 
 
+class FishInfo:
+    name_of_fish = "Bream"
+    price_in_uah_per_kilo = 70
+    is_fresh_due_date = datetime.date(2022, 2, 2) # datetime.date(year, month, date)
+    origin_of_fish = "Ukraine"
+    catch_date_of_fish = datetime.datetime(2022, 1, 30, 17, 35) # datetime.datetime(year, month, date, hour, minute...)
+
+    def __init__(self, name_of_fish: str = "", price_in_uah_per_kilo: str = "") -> None: # def__init__ - oleh = Student("oleh", "petrovych")
+        self.name_of_fish = name_of_fish
+        self.price_in_uah_per_kg = price_in_uah_per_kilo
+
+
+class FishBox:
+    roach = FishInfo("Roach", "86")
+
+    weight_of_fish_in_kilo = 12.4
+    package_date_of_fish = datetime.datetime(2022, 1, 29, 14, 29)
+
+    height_of_fish_box_in_m = 1.5
+    length_of_fish_box = 2.1
+    width_of_fish_box = 0.8
+    is_fish_alive_in_fish_box = bool(roach) # Example of using 'bool': bool_is_cool = 0 / print(bool(bool_is_cool)). Output: True
+
+
 class FishShop:
+    fish_boxes_dict = {
+        "2033": "Salmon",  # It's dictionary
+        "4012": "Carp",
+        "1988": "Swordfish"  # Structure: code_of_box(identity) : fish_name
+    }
+
+    # fresh_fish_dict = {
+    #     "Salmon": 120,
+    #     "Carp": 90,
+    #     "Cod": 105
+    # }
+
     name_of_fish_shop = "Svizhak"
     sentence_of_announcement_of_fish_shop = "The fish shop " + "'" + name_of_fish_shop + "'" + " is inviting you!"
     address_of_fish_shop = "Kopernyka street, 39"
@@ -64,16 +106,46 @@ class FishShop:
 
     print(sentence_of_announcement_of_fish_shop)
 
-    def add_fish(self, fish_name: str, total_weight: float) -> None:
-        pass
+    def __init__(self, name_of_fish) -> None:
+        self.name_of_fish = name_of_fish
 
-    def get_fish_names_sorted_by_price(self) -> List[Union[str, float]]:
-        pass
+    def add_fish(self, fish_name: str, total_weight: float) -> None:  # For class 'Fish'
+        self.fish_name = fish_name
+        self.total_weight = total_weight
 
-    def sell_fish1(self, fish_name: str, weight: float) -> float:
-        pass
+    def add_fish(self, fish_name: str, total_weight: float) -> None:  # For class 'FishBox'
+        self.fish_name = fish_name
+        self.total_weight = total_weight
 
-    def cast_out_old_fish(self) -> List[Union[str, float]]:
+    def get_fish_names_sorted_by_price(self, fresh_fish_dict, sorted_fresh_fish_dict) -> List[Union[str, float]]: # ?Possible realization of sorting
+        self.fresh_fish_dict = fresh_fish_dict
+
+        fresh_fish_dict = {
+            "Salmon": 120,
+            "Carp": 90,
+            "Cod": 105
+        }
+
+        self.sorted_fresh_fish_dict = sorted_fresh_fish_dict
+        sorted_fresh_fish_dict = sorted(fresh_fish_dict.items(), key = lambda x: x[1], reverse = True) # In descending order of values(example)
+        print(sorted_fresh_fish_dict)
+
+    def sell_fish(self, fish_name: str, weight_of_fish: float, is_fish_fresh: bool) -> Union[str, float, float]:
+        self.fish_name = fish_name
+        self.weight_of_fish = weight_of_fish
+        self.is_fish_fresh = is_fish_fresh
+
+    def get_frozen_fish_names_sorted_by_price(self, frozen_fish_names_sorted_by_price) -> list[str, bool, float]: # ?Tuple
+        self.frozen_fish_names_sorted_by_price = frozen_fish_names_sorted_by_price
+        print(self.frozen_fish_names_sorted_by_price)
+        pass  # I'm sorting too...
+
+    def get_fresh_fish_names_sorted_by_price(self, fresh_fish_names_sorted_by_price) -> list[str, bool, float]:
+        self.fresh_fish_names_sorted_by_price = fresh_fish_names_sorted_by_price
+        print(self.fresh_fish_names_sorted_by_price)
+        pass  # Sorting is hard...
+
+    def cast_out_old_fish(self) -> list[Union[str, float]]:
         pass
 
 
@@ -91,8 +163,11 @@ class Buyer:
 # taras.print_name()
 # vasyl = Dean()
 
-sold_salmon = Fish()
+sold_salmon = Fish("Salmon")
 sold_salmon.print_sold_fish("salmon")
 
-bought_tuna = Fish()
+bought_tuna = Fish("Tuna")
 bought_tuna.print_bought_fish("tuna")
+
+roach = FishShop("Roach")  # Example of method
+roach.add_fish("Roach", 0.4)  # (name, weight)
